be35fd7ae28536bffacedf15067072b3d9bb3690
Bannu <Bannu@LAPTOP-6O2UISIC>
2022-06-07 09:21:07 +0530

    First Commit

---
+++ src/test/abc.java
@@ -0,0 +1,14 @@
+package test;
+
+public class abc {
+
+	public abc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
